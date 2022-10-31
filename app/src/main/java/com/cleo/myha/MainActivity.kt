2ff1fa41fe0799a26d62e31f6604de4606e5ddd1
Cleo <vyiryic@gmail.com>
2022-10-31 17:53:24 +0800

    bottom navigation

--- app/src/main/java/com/cleo/myha/MainActivity.kt
+++ app/src/main/java/com/cleo/myha/MainActivity.kt
@@ -2,10 +2,39 @@ package com.cleo.myha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import com.cleo.myha.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.item_home -> {
+                    findNavController(R.id.my_nav_host_fragment).navigate(NavGraphDirections.actionGlobalHomeFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.item_habits -> {
+                    findNavController(R.id.my_nav_host_fragment).navigate(NavGraphDirections.actionGlobalHabitFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.item_discover -> {
+                    findNavController(R.id.my_nav_host_fragment).navigate(NavGraphDirections.actionGlobalDiscoverFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.item_profile -> {
+                    findNavController(R.id.my_nav_host_fragment).navigate(NavGraphDirections.actionGlobalProfileFragment())
+                    return@setOnItemSelectedListener true
+                }
+                else -> {return@setOnItemSelectedListener false}
+            }
+        }
+
     }
 }
