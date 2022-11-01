ad3c25a869624e26d641d0dcbac98505fcc63687
Cleo <vyiryic@gmail.com>
2022-11-01 17:53:09 +0800

    updated mockdata

--- app/src/main/java/com/cleo/myha/data/Habits.kt
+++ app/src/main/java/com/cleo/myha/data/Habits.kt
@@ -3,12 +3,10 @@ package com.cleo.myha.data
 data class Habits(
     var id: String,
     var ownerId: String,
-//    var members: Array<String>,
-    var members: String,
+    var members: List<String>,
     var category: String,
     var task: String,
-//    var repeatedDays: Array<Boolean>,
-    var repeatedDays: String,
+    var repeatedDays: List<Boolean>,
     var duration: String,
     var reminder: Long,
 //    var pomodoro: List<PomodoroData>
