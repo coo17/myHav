7b39bc51e2cdc91f306fc0b47efd547da95af4a1
Cleo <vyiryic@gmail.com>
2022-11-20 23:46:03 +0800

    fetch the user data

---
+++ app/src/main/java/com/cleo/myha/data/User.kt
@@ -0,0 +1,9 @@
+package com.cleo.myha.data
+
+data class User(
+    var id: String = "",
+    var name: String = "",
+    var photo: String = "",
+    var email: String = "",
+//    var bio: String
+)
