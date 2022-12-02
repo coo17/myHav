61d77361791ee0d66ac163c2b9f715fba5b01da3
Cleo <vyiryic@gmail.com>
2022-12-02 17:02:47 +0800

    updated selected event on main page

--- app/src/main/java/com/cleo/myha/home/HomeFragment.kt
+++ app/src/main/java/com/cleo/myha/home/HomeFragment.kt
@@ -4,20 +4,21 @@ package com.cleo.myha.home
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.applandeo.materialcalendarview.EventDay
 import com.cleo.myha.NavGraphDirections
-import com.cleo.myha.R
 import com.cleo.myha.databinding.FragmentHomeBinding
+import com.github.sundeepk.compactcalendarview.CompactCalendarView.CompactCalendarViewListener
+import com.github.sundeepk.compactcalendarview.domain.Event
 import com.google.firebase.auth.FirebaseAuth
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class HomeFragment : Fragment() {
@@ -56,23 +57,50 @@ class HomeFragment : Fragment() {
         })
 
 
-        val d1 = GregorianCalendar(2022,11,9)
-        val d2 = GregorianCalendar(2022,11,10)
-        val d3 = GregorianCalendar(2022,11,11)
-        val d4 = GregorianCalendar(2022,11,16)
-        val d5 = GregorianCalendar(2022,11,17)
-        val d6 = GregorianCalendar(2022,11,22)
-        val d7 = GregorianCalendar(2022,11,28)
+        val compactCalendar = Calendar.getInstance()
+        compactCalendar.set(2022,11,5)
+        val ev1 = Event(Color.GREEN, compactCalendar.timeInMillis, "Some extra data that I want to store.")
+        val ev2 = Event(Color.BLACK, compactCalendar.timeInMillis, "Some extra data that I want to store.")
 
-        val events : MutableList<EventDay> = ArrayList()
-        val calendar = Calendar.getInstance()
-        calendar.set(2022,10,10)
-        events.add(EventDay(calendar, R.drawable.ic_add))
+        binding.compactcalendarView.displayOtherMonthDays(true)
+        binding.compactcalendarView.setEventIndicatorStyle(1)
+//        binding.compactcalendarView.setEventIndicatorStyle(1)
+
+        binding.compactcalendarView.addEvent(ev1)
+        binding.compactcalendarView.addEvent(ev2)
+        Log.d("TAG", "today $ev1")
 
 
-        binding.calendarView.selectedDates = listOf(d1,d2,d3,d4,d5,d6,d7)
-        binding.calendarView.setEvents(events)
+        // define a listener to receive callbacks when certain events happen.
+       binding.compactcalendarView.setListener(object : CompactCalendarViewListener {
+            override fun onDayClick(dateClicked: Date) {
+                val events: List<Event> = binding.compactcalendarView.getEvents(dateClicked)
+                Toast.makeText(context,"Day was clicked: $dateClicked with events $events",Toast.LENGTH_SHORT).show()
+                Log.d("TAG", "Day was clicked: $dateClicked with events $events")
+            }
 
+            override fun onMonthScroll(firstDayOfNewMonth: Date) {
+                Log.d("TAG", "Month was scrolled to: $firstDayOfNewMonth")
+            }
+        })
+
+//        val d1 = GregorianCalendar(2022,11,9)
+//        val d2 = GregorianCalendar(2022,11,10)
+//        val d3 = GregorianCalendar(2022,11,11)
+//        val d4 = GregorianCalendar(2022,11,16)
+//        val d5 = GregorianCalendar(2022,11,17)
+//        val d6 = GregorianCalendar(2022,11,22)
+//        val d7 = GregorianCalendar(2022,11,28)
+//
+//        val events : MutableList<EventDay> = ArrayList()
+//        val calendar = Calendar.getInstance()
+//        calendar.set(2022,10,10)
+//        events.add(EventDay(calendar, R.drawable.ic_add))
+//
+
+//        binding.calendarView.selectedDates = listOf(d1,d2,d3,d4,d5,d6,d7)
+//        binding.calendarView.setEvents(events)
+//
 
 
         return binding.root
